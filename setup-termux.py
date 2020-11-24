3229dec8b20c5b1d9142da3b3f7bae157216f6f7
R2Boyo25 <R2Boyo25@gmail.com>
2020-11-24 15:53:00 -0600

    Improved readability in setup(s)

--- setup-termux.py
+++ setup-termux.py
@@ -1,34 +1,41 @@
 import os, runner
 
 def run(command):
+    
     runner.run(command, f"{command} run successfully!", f"{command} failed to run!")
 
 runner.checkreqs()
 
 print("-- Starting Installer --")
 
-#progs = ["git", "python", "openssh", "nano", "root-repo"]
-
 progs = []
 
 with open("./resources/installs", "r") as file:
+
     for line in file.readlines():
+
         if line != "\n":
+
             progs.append(line.replace("\n", ""))
 
 commands = []
 
 for program in progs:
+
     commands.append(f"pkg install -y {program}")
 
 for command in commands:
+
     run(command)
 
 print("Editing PS1 in bash.bashrc")
 
 with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "r") as file:
+
     if not "PS1='\w \$ '" in file.read():
+
         with open("/data/data/com.termux/files/usr/etc/bash.bashrc", "a") as file2:
+
             file2.write("\n"+r"PS1='\w \$ '")
 
 print("-- Done!--")
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import os
-from os import path
-
 
 pyfiles=[]
 
 print("- Looking For Files -")
 
 for root, dirs, files in os.walk("."):
+
     for file in files:
+
         print(file, end=" - ")
+
         if file.lower().endswith(".py") and file.split("/")[-1] not in ("setup.py", "setup-termux.py", "runner.py"):
+
             pyfiles.append(file)
+
     break
 
 print("")
@@ -19,40 +22,58 @@ print("-- Validating Files --")
 
 bashfile = "/data/data/com.termux/files/usr/etc/bash.bashrc" if os.path.exists("/data/data/com.termux/") else os.path.expanduser("~/.bashrc")
 
+print(f"-- Searching {pyfiles} --")
+
 with open(bashfile, "r") as bash:
-    print(f"-- Searching {pyfiles} --")
+
     bash2=bash.readlines()
 
 def inval(bash2):
-    a=0
     global pyfiles
+
+    a=0
+
     for file in pyfiles:
+
         print(f"Selecting {file}...")
+
         for line in bash2:
+
             line2=line.replace("\n", "")
+
             try:
+
                 if (os.path.abspath(file) in line and not line.startswith("#")):
+
                     print(f"{pyfiles.pop(pyfiles.index(file))} is invalid! Line: {line2}")
+
                 elif file.replace(".py", "") in line and not line.startswith("#"):
+
                     print(f"{pyfiles.pop(pyfiles.index(file))} is invalid! Line: {line2}")
-            except:a=a+1
-                   #print(f"{file} already invalidated!")
+            except:
+                pass
+
         print(f"Ending {file}...")
 
 for number in range(len(pyfiles)*2):
+
     inval(bash2)
  
 for file in pyfiles:
+
     print(f"{file} is valid!")
 
 print(f"--- Editing {bashfile} ---")
 
 with open(bashfile, "a") as bash:
-    if len(pyfiles)>0:
+
+    if len(pyfiles) > 0:
+
         bash.write("\n")
+
         for file in pyfiles:
+
             bash.write(f"alias {file.replace('.py', '')}=\"python3 {os.path.abspath(file)}\"\n")
-            #print(f"alias {file.replace('.py', '')}=\"python3 {os.path.abspath(file)}\" >> $PREFIX/etc/bash.bashrc\n")
 
 print("Done!")
 
