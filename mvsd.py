88e7d91d8262b9a5fa107a86765f124daf7a2939
R2Boyo25 <R2Boyo25@gmail.com>
2020-11-24 13:09:50 -0600

    added runner.py and added error handling

--- mvsd.py
+++ mvsd.py
@@ -1,9 +1,11 @@
-import os, sys
+import sys, runner
 
-count=0
+filesMoved=0
 
 for file in sys.argv[1:]:
-    os.system(f"mv -i {file} ~/storage/external-1/{file} 2> /dev/null")
-    print(f"Transferred {file}...")
-    count=count+1
-print(f"{count} files successfully transferred")
+    
+    runner.run(f"mv -i {file} ~/storage/external-1/{file} 2> /dev/null", f"Transferred {file}...", f"Failed to transfer {file}!")
+
+    filesMoved+=1
+
+print(f"{filesMoved} files successfully transferred")
